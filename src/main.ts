0f55ddacc5e98b16e444c3d44c712bd621868f61
ми на <152281018+mmnchkk@users.noreply.github.com>
2025-04-08 23:04:45 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,29 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', // Разрешаем все источники для разработки
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+
+  //  Получаем NODE_ENV из environment variables
+  const nodeEnv = process.env.NODE_ENV || 'development';
+
+  // Настраиваем CORS в зависимости от окружения
+  if (nodeEnv === 'production') {
+    app.enableCors({
+      origin: [
+        'https://m.vk.com/app53265826',
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    });
+  } else {
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    });
+  }
+
+
   await app.listen(3000);
 }
 bootstrap();
+
